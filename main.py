8b1461c87917fb768b7d6fb92ae8d664b402b0aa
bela <nagy.bela.budapest@gmail.com>
2023-05-10 21:12:55 +0200

    Második feladat : kiírás

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open('GordonRamsayRestorants.csv', encoding='utf-8') as f:
     lines = f.readlines()   # sorok beolvasása
     header = lines[0]     # a fejléc listába mentése, ez nem feladat
     rows = [line.strip().split('\t') for line in lines[1:]]  # A sorokat az 1-es indextől feldarabolom, tabulátorral
-    print(f"1. feladat : Beolvasás ... készen van! ({len(rows)} rekord)") #kiírom a minta szerint
+    print(f"1. feladat : Beolvasás ... készen van! ({len(rows)} rekord)") #kiírom a minta szerint, nincs behúzás
 
 # 2.Jelezze ki, összesen hány étteremmel került GR kapcsolatba!
 print(f'2. feladat: \tGordon Ramsey {len(rows)} étteremmel került kapcolatba') # egy behúzással kiírva a minta serint
