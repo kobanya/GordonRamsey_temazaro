dcff78c483074599d8b897de58f5ed0c28241d53
bela <nagy.bela.budapest@gmail.com>
2023-05-12 21:30:53 +0200

    kerekítés

--- main.py
+++ main.py
@@ -1,56 +1,74 @@
-#Nagy Béla 2023-05-15   Témazáró vizsga feladat
-import magyar
+# Nagy Béla 2023-05-15   Témazáró vizsga feladat
+
+import magyar  # sortörés, abc-be rendezés, kerekítés
+def beolvasas():
+    with open('GordonRamsayRestorants.csv', encoding='utf-8') as f:
+        lines = f.readlines()  # sorok beolvasása
+        fejlec = lines[0]  # a fejléc listába mentése, ez nem feladat
+        sorok = [line.strip().split('\t') for line in lines[1:]]  # A sorokat az 1-es indextől feldarabolom, tabulátorral
+        print(f"1. feladat : Beolvasás ... készen van! ({len(sorok)} rekord)")  # kiírom a minta szerint, nincs behúzás
+        return sorok
 
 def mentes_fajlba():
+
     with open('adatok.txt', 'w', encoding='utf-8') as f:
-        f.write(f"1. feladat : Beolvasás ... készen van! ({len(sorok)} rekord)\n")             # 1. feladat mentése
-        f.write(f'2. feladat: \tGordon Ramsey {len(sorok)} étteremmel került kapcolatba.\n')   # 2. feladat mentése
-        f.write(f"3. feladat: \tJelenleg  {muvekodo_ettermek_szama} étterem működik.\n")       # 3. feladat mentése
-        f.write(f"4. feladat: \tAz éttermek {zaras_arany:.0f} %-a zárt be\n")                  # 4. feladat mentése
-        f.write("5. feladat: \tMichlein-csillagot már kaptak az alábbi éttermek:\n")           # 5. feladat mentése
+        f.write(f"1. feladat : Beolvasás ... készen van! ({len(sorok)} rekord)\n")
+        f.write(f'2. feladat: \tGordon Ramsey {len(sorok)} étteremmel került kapcsolatba.\n')
+        f.write(f"3. feladat: \tJelenleg {muvekodo_ettermek_szama} étterem működik.\n")
+        f.write(f"4. feladat: \tAz éttermek {kerekitett_arany:.0f} %-a zárt be\n")
+        f.write("5. feladat: \tMichelin-csillagot már kaptak az alábbi éttermek:\n")
         f.write('\n'.join([f"\t\t{sor[0]:40.40} \t {sor[1]}" for sor in sorok if int(sor[5]) > 0]))
-        f.write("6. feladat: \tLegkevésbé sikeres éttermek adatai:\n")                          # 6. feladat mentése
+        f.write("6. feladat: \tLegkevésbé sikeres éttermek adatai:\n")
         f.write('\n'.join(eredmeny))
         f.write("\n7. feladat: \tA jelenleg is működő éttermek közül a legtöbb csillaga a következő(k)nek van:\n")
-        f.write('\n'.join(legjobb_ettermek_kimenet))                                            # 7. feladat mentése
-        f.write("\n8. feladat: \tGordon Ramsey a következő országokban nyitott éttermeket:\n")  # 8. feladat mentése
+        f.write('\n'.join(legjobb_ettermek_kimenet))
+        f.write("\n8. feladat: \tGordon Ramsey a következő országokban nyitott éttermeket:\n")
         f.write(formazott)
         f.write("\n9. feladat: \tÚjranyitott éttermek:\n")
         f.write('\n'.join(ujranyitott_ettermek_kimenet))
         return
 
+def mentes_menu():
+    print('--------------------------------------------------------------------------')
+    mentes = input("Szeretné fájlba menteni az adatokat? 1-igen, 2-nem: ")
+
+    while mentes not in ["1", "2"]:
+        mentes = input("Szeretné fájlba menteni az adatokat? 1-igen, 2-nem: ")
+
+    if mentes == "2":
+        print('Mentés nélkül kilépek.')
+
+    elif mentes == "1":
+        mentes_fajlba()
+        print('Az adatokat elmentettem! Nyissa meg az adatok.txt fájlt')
+
+sorok = beolvasas()
 
 
-# 1.Olvassa be egy alkalmas adatszerkezetbe az állomány tartalmát!
-with open('GordonRamsayRestorants.csv', encoding='utf-8') as f:
-    lines = f.readlines()                                                         # sorok beolvasása
-    fejlec = lines[0]                                                             # a fejléc listába mentése, ez nem feladat
-    sorok = [line.strip().split('\t') for line in lines[1:]]                      # A sorokat az 1-es indextől feldarabolom, tabulátorral
-    print(f"1. feladat : Beolvasás ... készen van! ({len(sorok)} rekord)")        #kiírom a minta szerint, nincs behúzás
 
 # 2.Jelezze ki, összesen hány étteremmel került GR kapcsolatba!
 print(f'2. feladat: \tGordon Ramsey {len(sorok)} étteremmel került kapcolatba.')  # egy behúzással kiírva a minta serint
 
 # 3.Írja ki a jelenleg is működő éttermek számát!
-aktualis_ev = 2023                                                                # az aktuális év
-muvekodo_ettermek_szama = 0                                                       # változó létrehozása
+aktualis_ev = 2023  # az aktuális év
+muvekodo_ettermek_szama = 0  # változó létrehozása
 
-for sor in sorok[1:]:                                                             # A fejléc sort kihagyjuk
-    if sor[4] == '0' or int(sor[4]) > aktualis_ev:                                # Nem zárt be, vagy az aktuális év után zárt be
-        muvekodo_ettermek_szama += 1                                              # a számláló megnövelése egyel
+for sor in sorok[1:]:  # A fejléc sort kihagyjuk
+    if sor[4] == '0' or int(sor[4]) > aktualis_ev:  # Nem zárt be, vagy az aktuális év után zárt be
+        muvekodo_ettermek_szama += 1  # a számláló megnövelése egyel
 print(f"3. feladat: \tJelenleg  {muvekodo_ettermek_szama} étterem működik.")
 
 # 4. Számolja ki, hány százalékuk zárt be az adatfelvétel időpontjáig!
-osszes_ettermek_szama = len(sorok)-1                                                            # A fejléc sort kihagyjuk
-zaras_arany = (osszes_ettermek_szama - muvekodo_ettermek_szama) / osszes_ettermek_szama * 100   # Az arány kiszámítása
-print(f"4. feladat: \tAz éttermek {zaras_arany:.0f} %-a zárt be")
+osszes_ettermek_szama = len(sorok) - 1  # A fejléc sort kihagyjuk
+zaras_arany = (osszes_ettermek_szama - muvekodo_ettermek_szama) / osszes_ettermek_szama * 100
+kerekitett_arany = magyar.fel_kerekit(zaras_arany)# Az arány kiszámítása
+print(f"4. feladat: \tAz éttermek {kerekitett_arany:.0f} %-a zárt be")
 
 # 5.Írja ki azokat az éttermeket és városaikat, amelyek kaptak már Michelin-csillagot!
 print(f"5. feladat: \tMichlein-csillagot már kaptak az alábbi éttermek:")
 for sor in sorok:
-    if int(sor[5]) > 0:                                                 # Ha legalább egy Michelin-csillaguk van
-        print(f"\t\t{sor[0]:40.40} \t {sor[1]}")                        # 40 karakter fenntartott hely
-
+    if int(sor[5]) > 0:  # Ha legalább egy Michelin-csillaguk van
+        print(f"\t\t{sor[0]:40.40} \t {sor[1]}")  # 40 karakter fenntartott hely
 
 # 6. Melyik éttermek voltak a legkevésbé sikeresek (nem kaptak csillagot és a legrövidebb ideig voltak nyitva)?
 print("6. feladat: \tLegkevésbé sikeres éttermek adatai:")
@@ -61,12 +79,13 @@ for sor in sorok:
         if len(legkevesbe_sikeresek) < 4:
             legkevesbe_sikeresek.append(sor)
         else:
-            legkevesbe_sikeresek.sort(key=lambda x: int(x[4])-int(x[3]))  # egy sor 4. indexű eleme
-            if int(sor[4])-int(sor[3]) < int(legkevesbe_sikeresek[-1][4])-int(legkevesbe_sikeresek[-1][3]):
+            legkevesbe_sikeresek.sort(key=lambda x: int(x[4]) - int(x[3]))  # egy sor 4. indexű eleme
+            if int(sor[4]) - int(sor[3]) < int(legkevesbe_sikeresek[-1][4]) - int(legkevesbe_sikeresek[-1][3]):
                 legkevesbe_sikeresek[-1] = sor
 
 eredmeny = [f'\t\t"{sor[0]}" {sor[1]} városában {sor[3]}-től {sor[4]}-ig volt nyitva.' for sor in legkevesbe_sikeresek]
 print('\n'.join(eredmeny))
+
 
 # 7. legjobb étterem
 def legjobb_ettermek():
@@ -79,8 +98,9 @@ def legjobb_ettermek():
                 legtobb_csillag_meg_mukodo = sor
 
     if legtobb_csillag_meg_mukodo is not None:
-        legjobb_ettermek_kimenet.append(f'\t\t"{legtobb_csillag_meg_mukodo[0]}" , amely {legtobb_csillag_meg_mukodo[1]} városában'
-                                       f' {legtobb_csillag_meg_mukodo[3]}. évben nyitott már {legtobb_csillag_meg_mukodo[5]} csillaga van')
+        legjobb_ettermek_kimenet.append(
+            f'\t\t"{legtobb_csillag_meg_mukodo[0]}" , amely {legtobb_csillag_meg_mukodo[1]} városában'
+            f' {legtobb_csillag_meg_mukodo[3]}. évben nyitott már {legtobb_csillag_meg_mukodo[5]} csillaga van')
     else:
         legjobb_ettermek_kimenet.append("Nincs adat a legtöbb csillaggal rendelkező és még működő étteremről 2023-ban.")
 
@@ -93,8 +113,7 @@ legjobb_ettermek_kimenet = legjobb_ettermek()
 for sor in legjobb_ettermek_kimenet:
     print(sor)
 
-
-#8. Sorolja fel névsorrendben, ismétlődés nélkül azokat az országokat, amelyekben működése alatt tevékenykedett a sztárszakács!
+# 8. Sorolja fel névsorrendben, ismétlődés nélkül azokat az országokat, amelyekben működése alatt tevékenykedett a sztárszakács!
 orszagok = set()  # Üres halmaz létrehozása az országok tárolására
 
 for sor in sorok:
@@ -105,9 +124,11 @@ orszagok_lista = list(orszagok)  # listává alakítjuk
 abc = magyar.abc(orszagok_lista)  # a magyar modul használata ÍRORSZÁG jó helyre helyezésére
 
 print("8. feladat: \tGordon Ramsey a következő országokban nyitott éttermeket:")
-formazott=magyar.ftordel(abc, 5,'\t\t')  # Szöveg tordeléseés kiírása
+formazott = magyar.ftordel(abc, 5, '\t\t')  # Szöveg tordeléseés kiírása
 print(formazott)
-#9.Volt-e újranyitott étterem ugyanabban a városban? Mikor és hol?
+
+
+# 9.Volt-e újranyitott étterem ugyanabban a városban? Mikor és hol?
 def ujranyitott_ettermek():
     ujranyitott_ettermek = []  # Üres lista az újranyitott éttermek tárolására
 
@@ -146,17 +167,6 @@ ujranyitott_ettermek_kimenet = ujranyitott_ettermek_kiiras()
 print("9. feladat: \tÚjranyitott éttermek:")
 print('\n'.join(ujranyitott_ettermek_kimenet))
 
-# 10 egy input használata és fájlba mentés:
 
-print('--------------------------------------------------------------------------')
-mentes = input("Szeretné fájlba menteni az adatokat? 1-igen, 2-nem: ")
 
-while mentes not in ["1", "2"]:
-    mentes = input("Szeretné fájlba menteni az adatokat? 1-igen, 2-nem: ")
-
-if mentes == "2":
-    print('Mentés nélkül kilépek.')
-
-elif mentes == "1":
-    mentes_fajlba()
-    print('Az adatokat elmentettem! Nyissa meg az adatok.txt fájlt')
+mentes_menu()
--- teszt.py
+++ teszt.py
@@ -1,11 +1,7 @@
-def tordel(lst, n=None, indent=None):
-    if n is None:
-        n = 10
-    if indent is None:
-        indent = ''
-    tordelt_sorok = []
-    for i in range(0, len(lst), n):
-        row = lst[i:i+n]
-        tordelt_sorok.append(f"{indent}{', '.join(map(str, row))}")
-    return "\n".join(tordelt_sorok)
+def le_kerekit(szam):
+    return int(szam // 1)
+
+eredmény= le_kerekit(0.9)
+
+print(eredmény)
 
